e083157e9ac6a6742face9c79867caecf7e92d47
Andra <andrijagrozdanovic@gmail.com>
2026-04-11 10:27:17 +0200

    Adding database injector for results and Race Card

--- DataFrame/DataFrame.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-
-
-def DateList(startDate, endDate):
-    Dates = [str(Date.strftime('%Y-%m-%d')) for Date in
-             pd.date_range(start=f'{startDate}', end=f'{endDate}').to_pydatetime().tolist()]
-    return Dates
-
